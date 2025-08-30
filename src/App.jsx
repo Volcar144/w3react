d5a05ed08c2623706ccef6108a71c05448ceac6b
Danng-Dev <futuretech.coder4@gmail.com>
2025-08-30 10:47:41 +0100

    React.dev Examples added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./User.jsx";
+import Profile from "./User.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function MyButton() {
+    return (
+        <button>I'm a button</button>
+    );
 }
 
-export default App
+const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+];
+
+const listItems = products.map(product =>
+    <li key={product.id}>
+        {product.title}
+    </li>
+);
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello World!</h1>
+            <h3>Profile Example</h3>
+            <Profile />
+            <hr />
+            <h3>Button Example</h3>
+            <MyButton />
+            <hr />
+            <h3>Dynamical Mapped list example</h3>
+            <ul>{listItems}</ul>
+        </div>
+    );
+}
+export default App;
